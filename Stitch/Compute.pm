ee8984c9111e7dda53ff2cf9adac7459c7e8be5a
Christian Garbs <mitch@cgarbs.de>
2021-01-24 12:41:01 +0100

    evenly distribute added and combined stitches

--- Stitch/Compute.pm
+++ Stitch/Compute.pm
@@ -34,20 +34,78 @@ sub _adjust_evenly {
 
     my $diff = $to - $from;
 
-    if ($diff > 0) {
+    my $actions = '';
 
-	return Stitch::Compute::List->new(elems => [
-					      Stitch::Compute::Keep->new(times => $from),
-					      Stitch::Compute::Add->new(times => $diff),
-					  ]);
+    my $grow   = $to > $from;
+    my $shrink = $to < $from;
 
+    my $oldpos = 0;
+    my $newpos = 0;
+    while ($oldpos < $from or $newpos < $to) {
+	my $diff = ( $oldpos ) - ($newpos / $to * $from);
+
+	if ($diff < 0 and $shrink) {
+	    $actions .= "C";
+	    $oldpos++;
+	    $oldpos++;
+	    $newpos++;
+	}
+	elsif ($diff > 0 and $grow) {
+	    $actions .= "A";
+	    $newpos++;
+	}
+	else {
+	    $actions .= "K";
+	    $oldpos++;
+	    $newpos++;
+	}
     }
-    else {
-	return Stitch::Compute::List->new(elems => [
-					      Stitch::Compute::Keep->new(times => $to + $diff),
-					      Stitch::Compute::Combine->new(times => -$diff),
-					  ]);
+
+    # normalize: balance head and tail
+    $actions =~ /^(([KCA])\2*)/;
+    my ($head, $head_what) = ($1, $2);
+    $actions =~ /(([KCA])\2*)$/;
+    my ($tail, $tail_what) = ($1, $2);
+
+    if (length $actions != $to) {
+	printf STDERR "\n*** %d != %d<%s>\n", $to, length $actions, $actions;
+	die "*** final length != expected length";
     }
+
+    if ($head_what eq $tail_what) {
+	my $head_length = length $head;
+	my $tail_length = length $tail;
+	# TODO: compute once instead of loop
+	while ($head_length > $tail_length) {
+	    $actions = substr($actions, 1) . $head_what;
+	    $head_length--;
+	    $tail_length++;
+	}
+	while ($head_length < $tail_length) {
+	    $actions = $tail_what . substr($actions, 0, -1);
+	    $head_length++;
+	    $tail_length--;
+	}
+    }
+
+    my @actions;
+    while ($actions =~ s/^(([KCA])\2*)//) {
+	my $count = length $1;
+	if ($2 eq 'K') {
+	    push @actions, Stitch::Compute::Keep->new(times => $count);
+	}
+	elsif ($2 eq 'C') {
+	    push @actions, Stitch::Compute::Combine->new(times => $count);
+	}
+	elsif ($2 eq 'A') {
+	    push @actions, Stitch::Compute::Add->new(times => $count);
+	}
+	else {
+	    die "unknown action type $2";
+	}
+    }
+
+    return Stitch::Compute::List->new(elems => \@actions);
 }
 
 1;
--- t/01.compute.t
+++ t/01.compute.t
@@ -1,5 +1,5 @@
 #!perl
-use Test::More tests => 7;
+use Test::More tests => 11;
 use warnings;
 use strict;
 
@@ -7,32 +7,52 @@ use Test::Exception;
 
 BEGIN { use_ok('Stitch::Compute') };
 
-subtest 'from 1 to 1' => sub {
+subtest 'keep: from 1 to 1' => sub {
     my $result = Stitch::Compute::adjust_stitches(1, 1);
     is($result, 'K1');
 };
 
-subtest 'from 50 to 50' => sub {
+subtest 'keep: from 50 to 50' => sub {
     my $result = Stitch::Compute::adjust_stitches(50, 50);
     is($result, 'K50');
 };
 
-subtest 'from 10 to 11' => sub {
+subtest 'add: from 10 to 11' => sub {
     my $result = Stitch::Compute::adjust_stitches(10, 11);
-    is($result, 'K10 A1');
+    is($result, 'K5 A1 K5');
 };
 
-subtest 'from 10 to 20' => sub {
+subtest 'add: from 10 to 20' => sub {
     my $result = Stitch::Compute::adjust_stitches(10, 20);
     is($result, '10x ( K1 A1 )');
 };
 
-subtest 'from 53 to 48' => sub {
-    my $result = Stitch::Compute::adjust_stitches(53, 48);
-    is($result, 'K43 C5');
+subtest 'add: from 70 to 112' => sub {
+    my $result = Stitch::Compute::adjust_stitches(70, 112);
+    is($result, '14x ( K1 A1 K1 A1 K2 A1 K1 )');
 };
 
-subtest 'from 30 to 25' => sub {
+subtest 'combine: from 53 to 48' => sub {
+    my $result = Stitch::Compute::adjust_stitches(53, 48);
+    is($result, 'K5 C1 K8 C1 K9 C1 K8 C1 K9 C1 K4');
+};
+
+subtest 'combine: from 30 to 25' => sub {
     my $result = Stitch::Compute::adjust_stitches(30, 25);
-    is($result, '5x ( K4 C1 )');
+    is($result, '5x ( K2 C1 K2 )');
+};
+
+subtest 'combine: from 8 to 5' => sub {
+    my $result = Stitch::Compute::adjust_stitches(8, 5);
+    is($result, 'K1 C2 K1 C1');
+};
+
+subtest 'combine: from 112 to 70' => sub {
+    my $result = Stitch::Compute::adjust_stitches(112, 70);
+    is($result, '14x ( K1 C2 K1 C1 )');
+};
+
+subtest 'combine: from 113 to 70' => sub {
+    my $result = Stitch::Compute::adjust_stitches(113, 70);
+    is($result, 'K1 C2 K1 C2 K1 C1 K1 C2 K1 C1 K1 C2 K1 C2 K1 C1 K1 C2 K1 C1 K1 C2 K1 C2 K1 C1 K1 C2 K1 C1 K1 C2 K1 C2 K1 C1 K1 C2 K1 C1 K1 C2 K1 C2 K1 C1 K1 C2 K1 C1 K1 C2 K1 C1');
 };
